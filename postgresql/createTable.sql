22211d61797b40b5e1bc008991e4d8a485c426cc
陆凌天 <1037192439@qq.com>
2019-10-21 14:23:52 +0800

    change primary key

--- postgresql/createTable.sql
+++ postgresql/createTable.sql
@@ -15,21 +15,21 @@ CREATE TABLE TrainSchedule(
     ts_tid             CHAR(6),
     ts_sid             integer,
     ts_seat_type       CHAR(8),
-    ts_part_TIME       TIME,
-    ts_arrival_TIME    TIME,
+    ts_part_time       TIME,
+    ts_arrival_time    TIME,
     ts_price           DECIMAL(18, 2),
     FOREIGN KEY (ts_tid) REFERENCES Train(t_tid),
     FOREIGN KEY (ts_sid) REFERENCES Station(ts_sid),
     FOREIGN KEY (ts_seat_type) REFERENCES Seat(seat_type),
-    PRIMARY KEY (ts_tid, ts_sid)
+    PRIMARY KEY (ts_tid, ts_sid, ts_seat_type)
 );
 
 CREATE TABLE TrainByDate(
     tbd_tid             CHAR(6),
     tbd_sid             INTEGER,
     tbd_seat_type       CHAR(8),
-    tbd_part_DATE       DATE,
+    tbd_part_date       DATE,
     tbd_seat_left       INTEGER,
     FOREIGN KEY (tbd_tid, tbd_seat_type) REFERENCES TrainSchedule(ts_tid, ts_sid, ts_seat_type),
-    PRIMARY KEY (tbd_tid, tbd_sid, tbd_part_DATE)
+    PRIMARY KEY (tbd_tid, tbd_sid, tbd_part_date, tbd_seat_type)
 );
