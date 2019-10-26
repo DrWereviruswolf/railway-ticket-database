68d9b5a616a62b8d5fbd555e15cbfd4d817d1671
陆凌天 <1037192439@qq.com>
2019-10-26 19:15:24 +0800

    update somebug

--- postgresql/createTable.sql
+++ postgresql/createTable.sql
@@ -1,5 +1,17 @@
+CREATE TYPE TrainType as enum(
+    'C',
+    'D',
+    'G',
+    'K',
+    'T',
+    'Y',
+    'Z'
+)
+--车次数有限
+
 CREATE TABLE Train(
-    t_tid       CHAR(6)
+    t_tid       CHAR(6),
+    t_tname     TrainType,
 );
 --WARNING
 
@@ -8,9 +20,12 @@ CREATE TABLE Seat(
 );
 
 CREATE TABLE Station(
-    s_sid       INTEGER
+    s_sid       INTEGER PRIMARY KEY,
+    s_sname     VARCHAR(10),
+    s_cityname  VARCHAR(10)
 );
 
+--Static data
 CREATE TABLE TrainSchedule(
     ts_tid             CHAR(6),
     ts_sid             integer,
@@ -19,11 +34,12 @@ CREATE TABLE TrainSchedule(
     ts_arrival_time    TIME,
     ts_price           DECIMAL(18, 2),
     FOREIGN KEY (ts_tid) REFERENCES Train(t_tid),
-    FOREIGN KEY (ts_sid) REFERENCES Station(ts_sid),
+    FOREIGN KEY (ts_sid) REFERENCES Station(s_sid),
     FOREIGN KEY (ts_seat_type) REFERENCES Seat(seat_type),
     PRIMARY KEY (ts_tid, ts_sid, ts_seat_type)
 );
 
+--Dynamic data
 CREATE TABLE TrainByDate(
     tbd_tid             CHAR(6),
     tbd_sid             INTEGER,
