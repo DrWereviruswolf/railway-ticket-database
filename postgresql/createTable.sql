f3865ad603091040af08c2e747d058536cb8c148
葒茶菌 <kouchyakun@gmail.com>
2019-10-26 20:35:23 +0800

    add some tables

--- postgresql/createTable.sql
+++ postgresql/createTable.sql
@@ -21,14 +21,14 @@ CREATE TABLE Seat(
 
 CREATE TABLE Station(
     s_sid       INTEGER PRIMARY KEY,
-    s_sname     VARCHAR(10),
-    s_cityname  VARCHAR(10)
+    s_sname     VARCHAR(20),
+    s_cityname  VARCHAR(20)
 );
 
 --Static data
 CREATE TABLE TrainSchedule(
     ts_tid             CHAR(6),
-    ts_sid             integer,
+    ts_sid             INTEGER,
     ts_seat_type       CHAR(8),
     ts_part_time       TIME,
     ts_arrival_time    TIME,
@@ -47,5 +47,31 @@ CREATE TABLE TrainByDate(
     tbd_part_date       DATE,
     tbd_seat_left       INTEGER,
     FOREIGN KEY (tbd_tid, tbd_seat_type) REFERENCES TrainSchedule(ts_tid, ts_sid, ts_seat_type),
-    PRIMARY KEY (tbd_tid, tbd_sid, tbd_part_date, tbd_seat_type)
+    PRIMARY KEY (tbd_tid, tbd_sid, tbd_seat_type, tbd_part_date)
+);
+
+CREATE TABLE User (
+  u_uid INTEGER PRIMARY KEY,
+  u_fullname CHAR(21),
+  u_idcard CHAR(19),
+  u_phone CHAR(12),
+  u_credit CHAR(17),
+  u_nick VARCHAR(32),
+  u_account DECIMAL(18, 2),
+  u_access SMALLINT
+);
+
+CREATE TABLE Reservation (
+  r_rid INTEGER PRIMARY KEY,
+  r_uid INTEGER,
+  r_tid CHAR(6),
+  r_part_sid INTEGER,
+  r_arrival_sid INTEGER,
+  r_seat_type CHAR(8),
+  r_part_date DATE,
+  FOREIGN KEY (r_uid) REFERENCES User(u_uid),
+  FOREIGN KEY (r_tid) REFERENCES Train(t_tid),
+  FOREIGN KEY (r_part_sid) REFERENCES Station(s_sid),
+  FOREIGN KEY (r_arrival_sid) REFERENCES Station(s_sid),
+  FOREIGN KEY (r_seat_type) REFERENCES Seat(seat_type)
 );
